93a358f3abe27f40c48ca6e41008d7c0d6ab91fa
RishiTheHindu <rishithehindu@gmail.com>
2026-03-15 23:58:36 +0530

    mere project me crud operation implement ho gayi hai

--- sms/student/urls.py
+++ sms/student/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("std/", views.student_home),
-    path("add-student/", views.add_student)
+    path("std/", views.student_home, name="student_home"),
+    path("add-student/", views.add_student, name="add_student"),
+
+    path("update-student/<int:student_id>", views.update_student, name="update_student"),
+
+    path("delete-student/<int:student_id>",views.delete_student, name="delete_student")
 ]
--- sms/student/views.py
+++ sms/student/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Student
 
@@ -17,6 +17,7 @@ def student_home(request):
 
 def add_student(request):
 
+
     if request.method == "POST" :
         #yaha hoga database me student save
         student_name = request.POST.get("input_name")
@@ -29,4 +30,35 @@ def add_student(request):
             phone_number = student_phone_number
         )
 
+        return redirect("student_home")
+
     return render(request, "student/add_student.html")
+
+def delete_student(request, student_id):
+
+    my_student = Student.objects.get(id = student_id)
+
+    my_student.delete()
+    
+    return redirect("student_home")
+
+def update_student(request, student_id):
+
+
+    student = Student.objects.get(id = student_id)
+
+    if request.method == "POST":
+
+        student.name = request.POST.get("name")
+        student.email = request.POST.get("email")
+        student.phone_number = request.POST.get("phone")
+
+        student.save()
+
+        return redirect("student_home")
+
+    parameters ={
+        "student": student
+    }
+    
+    return render(request, "student/update_student.html",parameters)
